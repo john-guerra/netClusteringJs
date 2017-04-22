dcfa64157303a40272c33ef606bc731049b27e57
John Alexis Guerra Gómez <john.guerra@gmail.com>
2017-04-22 12:00:07 -0400

    Adding an example to cluster locally

---
+++ clusterGraph.js
@@ -0,0 +1,25 @@
+graph = require("./data/ieeevisNetwork.json");
+netClustering = require("./js/netClustering.js");
+var jsonfile = require('jsonfile')
+
+var dictNodes = {};
+
+
+var filteredNodes = graph.nodes.filter(function (d) { return d.influential=== true; });
+filteredNodes = filteredNodes.concat(graph.nodes.filter(function (d) { return d.influential!== true; }).slice(0));
+
+filteredNodes.forEach(function (d) {
+	dictNodes[d.id] = d;
+});
+
+var links = graph.links.map(function (d) {
+	return {source:dictNodes[d.source], target:dictNodes[d.target]};
+});
+
+netClustering.cluster(graph.nodes, links);
+// graph.links = links;
+
+var file = './data/ieeevisNetworkClustered.json'
+jsonfile.writeFile(file, graph, function (err) {
+  console.error(err)
+})
